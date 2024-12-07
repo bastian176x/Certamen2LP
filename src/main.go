7cf4278ada4f425b05fec855531c5a1cfd7d3257
bastian176x <bastian.freire@alumnos.uv.cl>
2024-12-07 03:01:46 -0300

    Correcciones menores

--- src/main.go
+++ src/main.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
+	//Por ahora solo son pruebas
+	//../input/process_1.txt
+	// abrir archivo
+	arch, err := os.Open("../input/process_1.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer arch.Close()
 
-	fmt.Println("Hello, World!")
+	//crear dispatcher
+	d := Dispatcher{}
+	//crear proceso
+	p := Process{
+		ID:              1,
+		Estado:          "listo",
+		Program_counter: 0,
+		ESduracion:      0,
+	}
+	//ejecutar proceso
+	p.ejecutarProceso(arch.Name(), d)
 
 }
--- src/process1.go
+++ src/process1.go
@@ -9,13 +9,13 @@ import (
 
 type Process struct {
 	ID              int    // id del proceso
-	estado          string // listo, bloqueado, ejecutando.
-	program_counter int    // contador de programa
+	Estado          string // listo, bloqueado, ejecutando.
+	Program_counter int    // contador de programa
 	ESduracion      int    // duracion de la operacion de E/S
 }
 
 func (p *Process) finalizarProceso() {
-	p.estado = "finalizado"
+	p.Estado = "finalizado"
 }
 
 func (p *Process) ejecutarProceso(archivo string, d Dispatcher) {
@@ -36,10 +36,12 @@ func (p *Process) ejecutarProceso(archivo string, d Dispatcher) {
 			continue
 		}
 		if strings.Contains(line, "I") {
-			p.program_counter++
+			fmt.Println("Instrucción")
+			p.Program_counter++
 		}
 		if strings.Contains(line, "ES") {
-			p.program_counter++
+			fmt.Println("E/S")
+			p.Program_counter++
 			d.addProcessBloqueados(*p)
 		}
 		if strings.Contains(line, "F") {
